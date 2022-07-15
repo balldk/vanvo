b9b04fdce58d95643abd7b2fc1a8b4001d2c45d5
balldk <pr0f3ss0r.b@gmail.com>
2022-07-16 01:29:54 +0700

    feat: improve error interface

--- errorhandler/error.go
+++ errorhandler/error.go
@@ -2,6 +2,7 @@ package errorhandler
 
 import (
 	"bytes"
+	"strings"
 	"vila/token"
 
 	"github.com/fatih/color"
@@ -21,27 +22,32 @@ type Error struct {
 }
 
 type ErrorList struct {
-	Errors []Error
+	filepath string
+	input    string
+	Errors   []Error
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+func min(a int, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
 }
 
 func NewError(errType ErrorType, message string, line int, row int) Error {
 	return Error{Type: errType, Message: message, Line: line, Row: row}
 }
 
-func (e *Error) String() string {
-	var buf bytes.Buffer
-
-	red := color.New(color.FgHiRed).Add(color.Bold)
-	white := color.New(color.FgWhite)
-
-	red.Fprint(&buf, e.Type+": ")
-	white.Fprintln(&buf, e.Message)
-
-	return buf.String()
-}
-
-func NewErrorList() *ErrorList {
-	return &ErrorList{Errors: []Error{}}
+func NewErrorList(input string, filepath string) *ErrorList {
+	return &ErrorList{Errors: []Error{}, input: input, filepath: filepath}
 }
 
 func (eh *ErrorList) AddError(err Error) {
@@ -57,11 +63,42 @@ func (eh *ErrorList) Length() int {
 	return len(eh.Errors)
 }
 
-func (eh *ErrorList) String() string {
+func (el *ErrorList) String() string {
 	var buf bytes.Buffer
 
-	for _, err := range eh.Errors {
-		buf.WriteString(err.String())
+	red := color.New(color.FgHiRed)
+	blue := color.New(color.FgBlue)
+	green := color.New(color.FgHiGreen)
+	white := color.New(color.FgWhite)
+
+	lines := strings.Split(el.input, "\n")
+
+	if el.filepath != "" {
+		green.Fprintln(&buf, "-->", el.filepath)
+	}
+
+	for _, err := range el.Errors {
+
+		fromLine := max(1, err.Line-1)
+		toLine := min(err.Line, len(lines))
+
+		red.Fprint(&buf, err.Type+": ")
+		white.Fprintln(&buf, err.Message)
+
+		for i := fromLine - 1; i < toLine; i++ {
+			line := lines[i]
+			if i+1 == err.Line {
+				blue.Fprint(&buf, i+1, " | ")
+				white.Fprintln(&buf, line)
+				blue.Fprint(&buf, " ", " | ")
+				white.Fprint(&buf, strings.Repeat(" ", err.Row-1))
+				red.Fprintln(&buf, "^")
+			} else {
+				blue.Fprint(&buf, " ", " | ")
+				white.Fprintln(&buf, line)
+			}
+		}
+		buf.WriteString("\n")
 	}
 
 	return buf.String()
