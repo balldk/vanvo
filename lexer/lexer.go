79d9a93f70c96c1db1286b50586990af61d472dd
balldk <pr0f3ss0r.b@gmail.com>
2022-07-28 02:05:02 +0700

    fix: move endline token to next line

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -53,10 +53,11 @@ func (l *Lexer) AdvanceToken() token.Token {
 	case '"':
 		tok = l.newToken(token.String, l.consumeString())
 	case '\n':
-		tok = l.newSingleToken(token.Endline)
-		tok.Literal = l.consumeSpace()
 		l.line += 1
 		l.column = 0
+		tok = l.newSingleToken(token.Endline)
+		tok.Literal = l.consumeSpace()
+		tok.Column = 1
 	case 0:
 		tok = l.newToken(token.EOF, []rune{})
 	default:
